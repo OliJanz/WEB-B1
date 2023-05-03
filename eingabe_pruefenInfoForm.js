333882d86f1da1de604a951cd317bb65b9450c39
OliJanz <Oliverjanz1999@gmail.com>
2023-05-03 22:15:52 +0200

    JavaScript_funktioniert
    JavaScript_funktioniert, externe Datei, wird aufgerufen, ValidateForm() muss bezüglich ergänzungen angepasst werden

--- eingabe_pruefenInfoForm.js
+++ eingabe_pruefenInfoForm.js
@@ -4,12 +4,16 @@ function validateForm() {
   var vipInput = document.getElementById("vipInput").value;
   var fosInput = document.getElementById("fosInput").value;
   var stehInput = document.getElementById("stehInput").value;
+  console.log(vipInput);
+  console.log(fosInput);
+  console.log(stehInput);
+
 
   if (isNaN(vipInput) || isNaN(fosInput) || isNaN(stehInput)) {
-    alert("Bitte wählen Sie einen Sitzplatz aus.");
+    document.getElementById('einPlatz').style.display = 'block';
     return false;
   }
-
+  console.log("test12313423s");
   if (vipInput <= 0 && fosInput <= 0 && stehInput <= 0) {
     document.getElementById("einPlatz").style.display = "block";
     return false;
