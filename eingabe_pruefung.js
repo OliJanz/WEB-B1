07ba53bf6093874b339a7f80bf6cab3b7927cefe
OliJanz <Oliverjanz1999@gmail.com>
2023-05-10 12:19:32 +0200

    Update eingabe_pruefen.js
    Die Fehlermeldung wird bei erneutem Submit geprüft und die Fehlermeldung verschwindet, wenn die Eingabe den Kriterien erfüllt.

--- eingabe_pruefung.js
+++ eingabe_pruefung.js
@@ -7,40 +7,46 @@ function validateInput() {
   console.log(nachname);
   console.log(email);
 
-
-
-
+  var retval = true;
  
 
   // Vorname-Validierung
   const vornameRegex = /^[A-Z][a-z]{1,19}$/; // min. 2 Buchstaben, beginnt mit Großbuchstabe
   if (!vornameRegex.test(vorname)) {
     document.getElementById("falscherVorname").style.display = 'block';
-    return false;
+    
+    retval =  false;
+  }
+  else{
+    document.getElementById("falscherVorname").style.display = 'none';
   }
 
   // Nachname-Validierung
   const nachnameRegex = /^[A-Z][a-zØ-öø-ÿ]+([ -](?!-)[a-zø-ÿ]+)*$/; // min. 2 Buchstaben, kann mit Kleinbuchstabe beginnen, keine zwei Bindestriche hintereinander, keine Sonderzeichen
   if (!nachnameRegex.test(nachname)) {
     document.getElementById("falscherNachname").style.display = 'block';
-    return false;
+    retval =  false;
+  }
+  else{
+    document.getElementById("falscherNachname").style.display = 'none';
   }
 
  // Größenbeschränkung
  if (vorname.length > 20 || nachname.length > 20) {
   alert('Vorname und Nachname dürfen maximal 20 Zeichen lang sein.');
-  return false;
+  retval =  false;
 }
 
   // E-Mail-Validierung
   if (!validateEmail(email)){
     document.getElementById("falscheEmail").style.display = 'block';
-    return false;
+    retval = false;
   }
-return true;
+  return retval;
 }
 
 function validateEmail(email) {
     const emailRegex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
     return emailRegex.test(email);
   }
+
