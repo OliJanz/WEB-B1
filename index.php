e91ae256cc7b413a46b72048bdc238c0bdd71495
Milan Lukic <2122158@stud.hs-mannheim.de>
2023-06-06 14:23:51 +0200

    index datei als .Php erstellt

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php 
+		include 'header.php';
+        
+
+		$servername ="localhost";	// Von hier Datenbank Verbindung herstellen
+		$user = "web_b-1";			
+		$password = "Uo3oa7ac";
+		$db = "Web_b1";
+
+		$connection = new mysqli($servername, $user, $password, $db);
+
+		if($connection->connect_error){
+			die("Fehler bei der Connection.".$connection->connect_error); // bis hier ist die Anbindung der Datenbank
+		}
+		
+		$sql = "SELECT Bild FROM Event";
+        $result = $connection->query($sql);
+
+        if ($result->num_rows > 0) {
+           $counter = 1;
+            
+            while ($row = $result->fetch_assoc()) {
+
+                if($counter ==1){
+                    echo '<section id="focus"';
+                    echo '<a href="buchung.html"><img src="data:image/png;base64,'.base64_encode($row['Bild']).'"></a>';
+                    echo '</section>';
+                    echo '<section id="cardSection">';
+                }
+                
+                else {
+
+                
+                echo '<div class="card"><a href="buchung.html"><img src="data:image/png;base64,'.base64_encode($row['Bild']).'"></a></div>';
+                
+            }
+            $counter++;
+            }
+            echo '</section>';
+            
+        } else {
+            echo "Keine Bilder gefunden.";
+        }
+
+		
+		$connection->close();
+        include 'footer.php';
+		?>
+
